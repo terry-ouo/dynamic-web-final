6553a3edbfd99edb1118e0f59747cbd190c2c082
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2022-05-15 19:07:50 +0800

    function finall

--- web/login.php
+++ web/login.php
@@ -1,14 +1,17 @@
 <?php
-include_once ("connect_database.php");
-$account = addslashes($_POST['account']);
-$password = addslashes($_POST['password']);
+$database_name = "localhost";
+$database_table = "web1";
+$account = "webuser";
+$password = "webuser@413";
 global $database_connect_status;
-$sql = "select * from users where userName='$account' and password='$password'";
-echo $sql;
-$resultLogin = $database_connect_status->query($sql);
-if ($resultLogin > 0) {
+$database_connect_status = mysqli_connect($database_name, $account, $password, $database_table);
+$user_account = addslashes($_POST['account']);
+$user_password = addslashes($_POST['password']);
+$sql = "select * from users where account='$user_account' and password='$user_password'";
+$result = $database_connect_status->query($sql);
+$row = mysqli_num_rows($result);
+if ($row) {
     echo "登入成功";
 } else {
     echo "登入失敗";
 }
-closeConnect();
