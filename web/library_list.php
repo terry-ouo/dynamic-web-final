13444e0335ed905864d782768cc4cb3220d55f6f
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2022-05-15 17:20:39 +0800

    stage 1

--- web/library_list.php
+++ web/library_list.php
@@ -6,10 +6,20 @@
 </head>
 <body>
 <?php include("model.php") ?>
-<?php
-$db_link = mysqli_connect("localhost", "webuser", "webuser@413", "web1") or die("連接失敗 : " . mysqli_connect_error());
-$sql ="SELECT * FROM `library`";
-echo $sql;
-?>
+<div style="margin-left: 400px; margin-top: 50px">
+    <?php
+    $db_link = mysqli_connect("localhost", "webuser", "webuser@413", "web1") or die("連接失敗 : " . mysqli_connect_error());
+    $sql ="SELECT * FROM `library`";
+    $result = $db_link->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "書名: " . $row["name"]. " - 作者: " . $row["author"]. " 類型: " . $row["type"].  " 入庫日期: " . $row["date"]."<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    ?>
+</div>
 </body>
 </html>
